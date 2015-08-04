5898d9cfa25e71970c29381db61d0f19227f1aef
Chris O'Brien <chriso_brien@hotmail.com>
2015-08-04 18:53:00 +0100

    More changes for Azure hosting..

---
+++ Solutions/Provisioning.UX.App/Provisioning.Common/Utilities/UrlUtility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    /// <summary>
+    /// Static methods to modify URL paths.
+    /// </summary>
+    public static class UrlUtility
+    {
+        /// <summary>
+        /// Added by COB. Used to help translate a virtual path string to a physical path.
+        /// </summary>
+        /// <param name="urlToProcess">Virtual path with forward slashes.</param>
+        /// <returns>Physical path with backslashes.</returns>
+        public static string ReplaceForwardSlashesWithBackslashes(string urlToProcess)
+        {
+            return urlToProcess.Replace('/', '\\');
+        }
+    }
+}
